6ab1054544111f771fb11c6ff7ca3d992a99e433
arnaud muscatelli <arnaud.muscatelli+lewagon@gmail.com>
2026-08-19 15:28:59 +0200

    Conflict very solved.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
     resources :markers, only: [ :create, :new, :destroy ]
   end
   resources :movies, only: [ :index, :show ]
-  root "pages#home"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
