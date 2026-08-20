57347f7f8fc3c0320abe07b9286f528ae1fdd9db
mugisha ndutiye <mugisha.n.dev@gmail.com>
2026-08-20 18:31:39 +0200

    modified:   app/controllers/chats_controller.rb         new file:   app/controllers/messages_controller.rb         modified:   app/controllers/movies_controller.rb         modified:   app/controllers/pages_controller.rb         new file:   app/helpers/messages_helper.rb         modified:   app/models/chat.rb         new file:   app/views/chats/index.html.erb         new file:   app/views/chats/new.html.erb         modified:   app/views/chats/show.html.erb         modified:   app/views/pages/home.html.erb         modified:   config/routes.rb         new file:   db/migrate/20260820162309_add_user_to_chats.rb         modified:   db/schema.rb         new file:   test/controllers/messages_controller_test.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_20_135753) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_20_162309) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
   create_table "chats", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_chats_on_user_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -210,6 +212,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_20_135753) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chats", "users"
   add_foreign_key "lists", "users"
   add_foreign_key "markers", "lists"
   add_foreign_key "markers", "movies"
